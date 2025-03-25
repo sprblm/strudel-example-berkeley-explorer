8cc94ef571c18f701fcd6f2f994b95e95b81eb21
Phil D <pcdrummond@icloud.com>
2025-03-25 00:42:18 -0700

    Clean up imports in ErrorBoundary component

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -1,5 +1,5 @@
 import React, { Component, ErrorInfo, ReactNode } from 'react';
-import { Box, Typography, Button, Paper } from '@mui/material';
+import { Typography, Button, Paper } from '@mui/material';
 import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 
 interface Props {
