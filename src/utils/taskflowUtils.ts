3780c7d23c5c5afbeed2d0d81f6ee5e45ce7540b
Phil D <pcdrummond@icloud.com>
2025-03-26 12:54:37 -0700

    feat: add taskflowUtils for centralized config access

---
+++ src/utils/taskflowUtils.ts
@@ -0,0 +1,4 @@
+import { taskflow } from '../pages/search-repositories/_config/taskflow.config';
+
+export const getListConfig = () => taskflow.data.list;
+export const getCardFields = () => taskflow.pages.index.cardFields;
