9cd086e1b07b4c451e74232659977d2a5bdd4bc1
Phil D <pcdrummond@icloud.com>
2025-03-30 16:47:18 -0700

    Change style of navigating.

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,7 +1,6 @@
-import { AppBar, Toolbar, InputBase, IconButton, Stack } from '@mui/material';
+import { AppBar, Toolbar, InputBase, IconButton } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import React from 'react';
-import { SideSheet } from './SideSheet';
 import { useLocation } from 'react-router-dom';
 
 /**
@@ -14,27 +13,22 @@ export const TopBar: React.FC = () => {
   return (
     <AppBar position="static" color="primary">
       <Toolbar>
-        <Stack direction="row" sx={{ alignItems: 'center', flexGrow: 1 }}>
-          <SideSheet />
-          {showSearch && (
-            <InputBase
-              placeholder="Search..."
-              sx={{ 
-                ml: 2,
-                flexGrow: 1,
-                backgroundColor: 'rgba(255, 255, 255, 0.15)',
-                borderRadius: 1,
-                px: 1,
-                '&:hover': {
-                  backgroundColor: 'rgba(255, 255, 255, 0.25)',
-                },
-              }}
-              startAdornment={
-                <SearchIcon sx={{ color: 'inherit', mr: 1 }} />
-              }
-            />
-          )}
-        </Stack>
+        <InputBase
+          placeholder="Search..."
+          sx={{ 
+            ml: 2,
+            flexGrow: 1,
+            backgroundColor: 'rgba(255, 255, 255, 0.15)',
+            borderRadius: 1,
+            px: 1,
+            '&:hover': {
+              backgroundColor: 'rgba(255, 255, 255, 0.25)',
+            },
+          }}
+          startAdornment={
+            <SearchIcon sx={{ color: 'inherit', mr: 1 }} />
+          }
+        />
       </Toolbar>
     </AppBar>
   );
