a472bd073409f28c7a96025259d637619e0f4aa8
Phil D <pcdrummond@icloud.com>
2025-07-02 16:36:30 -0700

    feat: update router configuration
    - Add new routes for the Berkeley Explorer application
    - Define route parameters for dynamic routing
    - Update type definitions for better type safety

--- src/router.ts
+++ src/router.ts
@@ -1,17 +1,33 @@
 // Generouted, changes to this file will be overridden
 /* eslint-disable */
 
-import { components, hooks, utils } from '@generouted/react-router/client'
+import { components, hooks, utils } from '@generouted/react-router/client';
 
 export type Path =
-  | ``
+  | `/`
+  | `/about`
+  | `/about/UrbanEnvironmentalHealth`
+  | `/compare-data`
+  | `/compare-data/compare`
+  | `/compare-data/new`
+  | `/contribute`
+  | `/explore-data`
+  | `/explore-data/:id`
+  | `/home`
+  | `/search-repositories`
+  | `/search-repositories/:id`;
 
 export type Params = {
-  
-}
+  '/explore-data/:id': { id: string };
+  '/search-repositories/:id': { id: string };
+};
 
-export type ModalPath = never
+export type ModalPath = never;
 
-export const { Link, Navigate } = components<Path, Params>()
-export const { useModals, useNavigate, useParams } = hooks<Path, Params, ModalPath>()
-export const { redirect } = utils<Path, Params>()
+export const { Link, Navigate } = components<Path, Params>();
+export const { useModals, useNavigate, useParams } = hooks<
+  Path,
+  Params,
+  ModalPath
+>();
+export const { redirect } = utils<Path, Params>();
